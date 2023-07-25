5a556b0bc0b9f1c3cfb9f4de4f553679afedcdfa
Dastan <ermekoffdastan@gmail.com>
2023-07-25 17:11:16 +0600

    'add'

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from api.login_handler import login_router
 app = FastAPI()
 
 main_api_router = APIRouter()
-main_api_router.include_router(login_router, prefix="login", tags=["login"])
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+main_api_router.include_router(login_router, prefix="/login", tags=["login"])
 
 app.include_router(main_api_router)
 
