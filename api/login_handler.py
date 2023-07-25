5051609fd3c57ca574718ef24676a43bbc4be835
jaman-bala <ermekoffdastan@yandex.ru>
2023-07-26 00:12:38 +0600

    'addingnewinfo'

--- api/login_handler.py
+++ api/login_handler.py
@@ -52,4 +52,34 @@ async def login_for_access_token(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
 
+
+async def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        print("username/email extracted is ", email)
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await _get_user_by_email_for_auth(email=email, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@login_router.get("/test_auth_endpoint")
+async def sample_endpoint_under_jwt(
+    current_user: User = Depends(get_current_user_from_token),
+):
+    return {"Success": True, "current_user": current_user}
